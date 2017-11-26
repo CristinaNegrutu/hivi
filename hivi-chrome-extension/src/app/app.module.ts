1df2ecced530555c6e01b5b20bd094019fcc3f2e
denise <denise.goldan@gmail.com>
2017-11-26 17:34:14 +0200

    Started working on side-menu component

--- hivi-chrome-extension/src/app/app.module.ts
+++ hivi-chrome-extension/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { ViewsParentComponent } from './views/views-parent.component';
-
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-		ViewsParentComponent
+		SideMenuComponent
   ],
   imports: [
     BrowserModule
