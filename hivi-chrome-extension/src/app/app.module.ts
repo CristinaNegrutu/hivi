19fbc474f8e5c933d6e038546a3c526b7c8efcef
Cristina Negrutu <cristina.negrutu96@gmail.com>
2017-11-26 19:03:49 +0200

    made first components

--- hivi-chrome-extension/src/app/app.module.ts
+++ hivi-chrome-extension/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { DataFiltersComponent } from './data-filters/data-filters.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
-		SideMenuComponent
+		SideMenuComponent,
+    DataFiltersComponent,
+    MainContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
