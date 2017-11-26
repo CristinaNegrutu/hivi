9eadaffbb07d2dadcbe2fbca149fc047a7a7db98
denise <denise.goldan@gmail.com>
2017-11-26 19:05:13 +0200

    Still working on side-nav

--- hivi-chrome-extension/src/app/app.module.ts
+++ hivi-chrome-extension/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
 		SideMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hivi-chrome-extension/src/app/side-menu/side-menu.component.ts
+++ hivi-chrome-extension/src/app/side-menu/side-menu.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
 	selector: 'side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+	templateUrl: './side-menu.component.html',
+	styleUrls: ['./side-menu.component.css']
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit(): void {
+
+	}
+
+	showSideMenu(): void {
+		$(".button-collapse").sideNav();
+		$('.button-collapse').sideNav('show');
+		console.log("Menu button has been pressed.")
+	}
 
 }
